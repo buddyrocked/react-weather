ea255df7e86a400c77a45137bf27844c4bcb4934
Budi Hariyana <budihariyana@gmail.com>
2017-12-21 16:51:28 +0700

    api openmapweather

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const API_KEY = '283da3ceb6aaf43e92b773d6199e5d4f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(){
+	return (
+		type: FETCH_WEATHER
+	);
+}
