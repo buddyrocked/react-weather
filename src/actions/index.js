315881e406939d541a7de49d581257c603c21967
budihariyana <budihariyana@gmail.com>
2017-12-22 06:39:01 +0700

    axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '283da3ceb6aaf43e92b773d6199e5d4f';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
-	const url = 
-	return (
-		type: FETCH_WEATHER
-	);
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
 }
